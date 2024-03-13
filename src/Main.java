c85fb6c0e5ff95822c578d2a6cc2a3f8b1097687
Şule Nehir ARAÇ <nehir.2706@gmail.com>
2024-03-13 11:19:51 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Kullanıcıdan bir tam sayı ve bir ondalıklı sayı al
+        System.out.print("Bir tam sayı girin: ");
+        int tamSayi = scanner.nextInt();
+
+        System.out.print("Bir ondalıklı sayı girin: ");
+        double ondalikliSayi = scanner.nextDouble();
+
+        // Tam sayıyı ondalıklı sayıya dönüştür ve ekrana yazdır
+        double d = (double) tamSayi;
+        System.out.println("Tam sayıyı ondalıklı sayıya dönüştürdük: " + d);
+
+        // Ondalıklı sayıyı tam sayıya dönüştür ve ekrana yazdır
+        int i = (int) ondalikliSayi;
+        System.out.println("Ondalıklı sayıyı tam sayıya dönüştürdük: " + i);
+    }
 }
